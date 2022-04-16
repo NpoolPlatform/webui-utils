cc078287655eed4e2a3118d3e8744e996bfa5962
Zhao KK <kikakkz@hotmail.com>
2022-04-16 14:15:06 +0000

    New webui tools

--- message.py
+++ message.py
@@ -3,11 +3,14 @@
 
 import sys
 import json
+import os
 
 
 def main(argv):
     msg_state = {}
-    language = ''
+    language = {}
+    old_msgs = {}
+    old_msg = {}
 
     with open(argv[0]) as msg_src_file:
         lines = msg_src_file.readlines()
@@ -18,15 +21,30 @@ def main(argv):
                     msg_state[msg] = 1
 
     with open(argv[1]) as msg_dst_file:
-        msgs = json.load(msg_dst_file)
-        language = msgs['Language']['Lang']
-        for msg in msgs['Messages']:
+        old_msgs = json.load(msg_dst_file)
+        language = old_msgs['Language']
+        for msg in old_msgs['Messages']:
+            old_msg = msg
             msg_state[msg['MessageID']] = 2
 
     print('Language: ', language)
+    msgs = old_msgs['Messages']
+
     for k, v in msg_state.items():
         if v != 2:
             print('Missed item: ', k, v)
+            msgs.append({
+                'AppID': old_msg['AppID'],
+                'LangID': old_msg['LangID'],
+                'MessageID': k,
+                'Message': 'TO BE FILLED',
+                'BatchGet': True
+            })
+
+
+    old_msgs['Messages'] = msgs
+    with open('new-' + os.path.basename(argv[1]), 'w') as new_msg_file:
+        new_msg_file.write(json.dumps(old_msgs, indent=4))
 
 
 if __name__ == '__main__':
