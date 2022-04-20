11f38c3bcb21313eb8f33dda358d231cda87c8c7
Zhao KK <kikakkz@hotmail.com>
2022-04-20 08:29:00 +0000

    Get message list

---
+++ message_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import sys
+import json
+import os
+
+
+def main(argv):
+    msg_state = {}
+
+    with open(argv[0]) as msg_src_file:
+        lines = msg_src_file.readlines()
+        for line in lines:
+            msgs = line.split("'")
+            for msg in msgs:
+                if msg.startswith('MSG_'):
+                    msg_state[msg] = 1
+
+    with open('new-' + os.path.basename(argv[0]), 'w') as new_msg_file:
+        new_msg_file.write(json.dumps(msg_state, indent=4))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
